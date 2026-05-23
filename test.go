e8f956b82d8a69c9adcc5e728c8bd19d8fe84c1a
bagget777 <bagget7777@gmail.com>
2026-05-23 23:23:06 +0600

    update 1

--- test.go
+++ test.go
@@ -31,9 +31,46 @@ func main() {
 			fmt.Println("succes", arr2[i])
 		}
 	}
-	sum2 := 0
 	arr3 := []int{18, 22, 15, 31, 25, 19}
+	max := arr3[0]
 	for i := 0; i < len(arr3); i++ {
-		if arr3[i] 
+		if arr3[i] > max {
+			max = arr3[i]
+		}
+	}
+	fmt.Println(max)
+
+	arr4 := []string{"Excelent", "Good", "Bad", "Good", "Excelent", "Bad", "Good"}
+	/* 
+	var good string
+	for i := 0; i < len(arr4); i++ {
+		if arr4[i] == "Good" {
+			good = arr4[i]
+		fmt.Println(len(good))
+		}
+	}
+	fmt.Println(len(good))
+	*/
+	endCount := 0
+	for i := 0; i < len(arr4); i++ {
+		countNumb := 0
+		if arr4[i] == "Good" {
+			countNumb++
+		}
+		endCount = endCount + countNumb
+	}
+	fmt.Println(endCount)
+	
+	arr5 := []int{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}
+	for _, v := range arr5 {
+		if v % 3 == 0 {
+			continue 
+		}
+		fmt.Println(v)
+	}
+
+	for i, v := range arr5 {
+		fmt.Println(v)
+		fmt.Println(i)
 	}
 }
